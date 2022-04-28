2f916383c28f1b0ad171f6273ecdff23a47598d7
NaveenGunturu <naveen.gunturu0011@gmail.com>
2022-04-28 22:07:40 +0530

    new-commit

--- app.py
+++ app.py
@@ -3,5 +3,13 @@ app=Flask(__name__)
 @app.route('/')
 def index():
   return "Hello World"
+
+@app.route("/number")
+def randomnum():
+    import random
+    from random import randint
+
+    a=random.randint(100,200)
+    return str(a)
 if __name__=="__main__":
-  app.run(debug=True)
+  app.run(debug=True,host="0.0.0.0")
